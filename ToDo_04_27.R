23d65fbd1db99558be0a8611658ef66dec3501e0
mhedwards <edwarmat@onid.orst.edu>
2014-04-27 20:52:09 -0700

    Update to do list.

---
+++ ToDo_04_27.R
@@ -0,0 +1,5 @@
+## To Do 4/27
+
+#Simulate data using Ramsey's density estimates, and detecition distances as an estimates of detection probability.
+#Use Buckland & Quang methods to analyze and see if we get close to those estimates.
+# report on closeness of estimates and variances.
