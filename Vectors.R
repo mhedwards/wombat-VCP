79925ca226b4e52eced5d4cd282120c20777f598
mhedwards <edwarmat@onid.orst.edu>
2014-05-20 19:36:23 -0700

    brainstorming about Vectors.

---
+++ Vectors.R
@@ -0,0 +1,17 @@
+## mainly notes to me about vector.
+
+From C and x change can get angle.
+
+From angle and new C can get x and y changes.
+
+Only necessary for the "compounded" version
+
+
+need to keep Original version somewhere & "current" x.y
+
+
+chance to move would be dnorm() based on original R.j
+then randomly pick distance. Weighted?
+
+for the "compounded" version, base next change off the updated x.y
+so, will need to have a "movement" variable: no, temp, compounded
