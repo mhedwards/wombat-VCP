f6198f3ee4e846847d6cc9ecc4802da7666128d0
mhedwards <edwarmat@onid.orst.edu>
2014-05-23 08:58:54 -0700

    Brainstorming

---
+++ fiddling/VCPvsLine.R
@@ -0,0 +1,4 @@
+
+# E(n) = D*Area*Probability of observing something in that area. 
+
+# I suspect that with the line transect, we'll see the nice shoulder curve that we are told to expect, but that with the circular plot, we'll see something that's lower near 0, simply due to the nature of the area of circles
